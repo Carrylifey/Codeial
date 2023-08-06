820e705553ff7d06fbc3afe158b5c0106323b75f
Subhankar Saha <subhankarsahacse9@gmail.com>
2023-08-06 23:36:23 +0530

    routes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ const homeController=require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+//for any other routes use from here 
+//router.use('/routername ,require(('./routerfilebyname')))
 
 
 module.exports=router;
