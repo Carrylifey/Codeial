e464d7a172b161608bc3e1737c8a959f583a6cc3
Subhankar Saha <subhankarsahacse9@gmail.com>
2023-08-08 02:38:14 +0530

    setup static files  access for layouts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,9 @@
 module.exports.profile=function(req,res){
-    res.end ('<h1>user profile</h1>')
-}
+    // res.end ('<h1>user profile</h1>')
+    return res.render('user_profile',{
+        title:"profile"
+    });
+};
+
+
+
