610ef7e0e1b1bf56e7824b434e2c98143f4e9eab
Subhankar Saha <subhankarsahacse9@gmail.com>
2023-08-11 05:21:17 +0530

    passport

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,39 +1,36 @@
 const user = require("../models/user");
 
-module.exports.profile=function(req,res){
-    // res.end ('<h1>user profile</h1>')
-    return res.render('user_profile',{
-        title:"profile"
-    });
+module.exports.profile = function (req, res) {
+  // res.end ('<h1>user profile</h1>')
+  return res.render("user_profile", {
+    title: "profile",
+  });
 };
 
-module.exports.signup = function(req,res){
-    return res.render('user_sign_up',{
-        title : "codeial | Sign UP "
-    })
-}
+module.exports.signup = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "codeial | Sign UP ",
+  });
+};
 //render the sign in page
-module.exports.signin = function(req,res){
-    return res.render('user_sign_in',{
-        title : "codeial | Sign In "
-    })
-}
+module.exports.signin = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "codeial | Sign In ",
+  });
+};
 
 //get the signup data
 // module.exports.create =function(req,res){
 //     if(req.body.password!=req.body.confirm_password){
-//        
 //         return res.redirect('back')
-        
 //     }
-
 //      user.findOne({email:req.body.email},
 //         function(err,user){
 //            if(err){
 //             console.log('error in finding user in signing up');
 //             return;
 //            }
-                
+
 //            if(!user){
 //             user.create(req.body,function(err,user){
 //                     if(err){console.log('error in creating while signing up');
@@ -45,36 +42,29 @@ module.exports.signin = function(req,res){
 //            }else{
 //             return res.redirect('back');
 //            }
-
-            
 //         })
-
-
 // }
 
+module.exports.create = async function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
 
-module.exports.create = async function(req, res) {
-    if (req.body.password != req.body.confirm_password) {
-        return res.redirect('back');
+  try {
+    const newUser = await user.findOne({ email: req.body.email });
+
+    if (!newUser) {
+      const newUser = await user.create(req.body);
+      return res.redirect("./sign-in");
+    } else {
+      return res.redirect("back");
     }
-
-    try {
-        const newUser = await user.findOne({ email: req.body.email });
-
-        if (!newUser) {
-            const newUser = await user.create(req.body);
-            return res.redirect('./sign-in');
-        } else {
-            return res.redirect('back');
-        }
-    } catch (err) {
-        console.log('Error in signing up:', err);
-        return res.redirect('back');
-    }
+  } catch (err) {
+    console.log("Error in signing up:", err);
+    return res.redirect("back");
+  }
 };
 
-
-module.exports.createSession = function(req,res){
-    //todolater
-}
-
+module.exports.createSession = function (req, res) {
+  //todolater
+};
--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-const express= require('express');
-const cookieParser= require('cookie-parser');
-const app=express();
-const port=8000;
-const expressLayouts= require('express-ejs-layouts');
-const db= require('./config/mongoose');
-
-
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
 app.use(express.urlencoded());
 app.use(cookieParser());
-app.use(express.static('./assets'))
+app.use(express.static("./assets"));
 app.use(expressLayouts);
 //extract style and scripts from subpages into the layout
-app.set('layout extractStyles',true)
-app.set('layout extractScripts',true)
-
-
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //use express router
-app.use('/',require('./routes'));
-
+app.use("/", require("./routes"));
 
 //setup the  view engine
-app.set('view engine','ejs')
-app.set('views','./views')
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-
-
-
-app.listen(port,function(err){
-    if(err){
-        console.log('Error: ',err);
-        console.log(`Error in runnning the server :${err}`)
-    }
-     console.log(`Server is running on port:${port}`);
-}) 
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error: ", err);
+    console.log(`Error in runnning the server :${err}`);
+  }
+  console.log(`Server is running on port:${port}`);
+});
