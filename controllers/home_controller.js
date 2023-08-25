5a50b7610a41979c4009a45776e6041e96438617
Subhankar Saha <subhankarsahacse9@gmail.com>
2023-08-25 21:48:49 +0530

    Posts populate

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,23 @@
-module.exports.home=function(req,res){
-    // return res.end('<h1>Express is up for codeial</h1>');
-    // console.log(req.cookies);
-    // res.cookie('user_id',25);
-    return res.render('home',{
-        title:"Home"
-    });
-};
+const Post= require('../models/post');
 
+
+module.exports.home = async function(req, res) {
+
+      
+            try {
+                const Posts = await Post.find({}).populate('user').exec();
+                return res.render('home', {
+                    title: "Codeial | Home",
+                    posts: Posts,
+                    user: 'user'
+                });
+            } catch (err) {
+                console.error(err);
+                res.status(500).send('Internal Server Error');
+            }
+        
+        
+
+
+
+};
--- models/user.js
+++ models/user.js
@@ -24,4 +24,4 @@ const userSchema = new mongoose.Schema({
 const user = mongoose.model('user',userSchema);
 
 
-module.exports=user;
+module.exports= user;
